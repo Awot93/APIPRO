b583d16c2b47370eec40a63aff36a837b5160a48
Awot <oladunjoye.wt@gmail.com>
2022-07-03 14:50:58 +0100

    a

--- I4G031897MHL/links/urls.py
+++ I4G031897MHL/links/urls.py
@@ -5,8 +5,8 @@ from .views import PostListApi, PostCreateApi, PostDetailApi, PostUpdateApi, Pos
 app_name="link"
 
 urlpatterns = [
-    path("create/", PostCreateApi.as_view(), name="api_create"),
-    path("update/<int:pk>", PostUpdateApi.as_view(), name="api_update"),
-    path("delete/<int:pk>", PostDeleteApi.as_view(), name="api_delete"),
-    path("", PostListApi.as_view(), name="api_list"),
+    path("create/", PostCreateApi.as_view({'get': 'list'}), name="api_create"),
+    path("update/<int:pk>", PostUpdateApi.as_view({'get': 'list'}), name="api_update"),
+    path("delete/<int:pk>", PostDeleteApi.as_view({'get': 'list'}), name="api_delete"),
+    path("", PostListApi.as_view({'get': 'list'}), name="api_list"),
 ]
